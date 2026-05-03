17d2b6ac96ce32468c22b0948493843910ec7d31
AliaHarb <aliaharb940@gmail.com>
2026-05-03 03:19:03 +0300

    Integrated video processing and image enhancement into main

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import image_enhancement
 if __name__ == "__main__":
     
     print("--- Lecture Archiving System: Execution Finished ---")
-    print("--- Check 'Outputs/Enhanced_Photos' for the final results ---")
+    print("--- Check 'Outputs' for the final results ---")
+    
